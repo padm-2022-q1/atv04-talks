496cfeff91219460ae092d75b4c27b7195ec45c3
Diogo S. Martins <santana.martins@ufabc.edu.br>
2022-05-15 11:15:44 -0300

    updated dataset with smaller files

--- app/src/main/java/br/edu/ufabc/talks/model/Dataset.kt
+++ app/src/main/java/br/edu/ufabc/talks/model/Dataset.kt
@@ -16,10 +16,10 @@ object Dataset {
             Video.parseDate("Mar 2015"),
             "Nathalie Cabrol",
             "https://www.ted.com/talks/nathalie_cabrol_how_mars_might_hold_the_secret_to_the_origin_of_life",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/NathalieCabrol_2015-480p.jpg?alt=media&token=3df86d5d-c85c-43db-ae15-2db796daa4fb",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/2015-nathalie-cabrol-011-5000k.webm?alt=media&token=7884060e-71be-432c-95b7-021c758dd8af",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "NathalieCabrol_2015-480p.jpg?alt=media&token=031bc014-0093-4f9b-a39c-2ff04ffd1995",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "NathalieCabrol_2015-480p_small.webm?alt=media&token=e691b15f-1892-4d62-a1e3-26b3f71c6e01",
         ),
         VideoDTO(
             2,
@@ -29,11 +29,10 @@ object Dataset {
             Video.parseDate("Oct 2014"),
             "Eduardo Sáenz de Cabezón",
             "https://www.ted.com/talks/eduardo_saenz_de_cabezon_math_is_forever",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/EduardoSaenzdeCabezon_2014X-480p.jpg?alt=media&token=40e6c8e9-98c6-4a31-bb91-161d1edc96f9",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/2014-eduardo-saenz-de-cabezon-002-5000k.webm?" +
-                "alt=media&token=0cf4c58a-3339-4695-b444-f4438f568f86",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "EduardoSaenzdeCabezon_2014X-480p.jpg?alt=media&token=e63f04b7-80d9-40d0-a1d4-87fa1aa43bd3",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "EduardoSaenzdeCabezon_2014X-480p_small.webm?alt=media&token=d49bc62a-724f-4261-a6a7-1fac6df8d966",
         ),
         VideoDTO(
             3,
@@ -43,10 +42,10 @@ object Dataset {
             Video.parseDate("Mar 2015"),
             "Joseph DeSimone",
             "https://www.ted.com/talks/joe_desimone_what_if_3d_printing_was_25x_faster",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/JosephDeSimone_2015-480p.jpg?alt=media&token=8fc2c5b9-0a8a-46b3-b4e6-b743edd3b6de",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/2015-joseph-desimone-006-5000k.webm?alt=media&token=a26bcf52-1b4d-4e3d-a9b6-eb0e3f51cae3",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "JosephDeSimone_2015-480p.jpg?alt=media&token=452775f5-a512-4af9-ad87-3082f0a4234b",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "JosephDeSimone_2015-480p_small.webm?alt=media&token=35d65403-3fdb-4f01-83a5-ebe986df1582",
         ),
         VideoDTO(
             4,
@@ -56,10 +55,10 @@ object Dataset {
             Video.parseDate("Oct 2014"),
             "Andy Yen",
             "https://www.ted.com/talks/andy_yen_think_your_email_s_private_think_again",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/AndyYen_2014G-480p.jpg?alt=media&token=b6d123ba-b4a2-44e7-9169-88793f978eab",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/2014-andy-yen-009-5000k.webm?alt=media&token=4faa537d-7c8e-4cf2-a8e7-48b8f73fa76c",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "AndyYen_2014G-480p.jpg?alt=media&token=a79c7ea5-a721-46ad-af53-a8915a6928d1",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "AndyYen_2014G-480p_small.webm?alt=media&token=c9d53212-dc60-461f-91e5-7fbad720da71",
         ),
         VideoDTO(
             5,
@@ -69,10 +68,10 @@ object Dataset {
             Video.parseDate("Mar 2015"),
             "Chris Milk",
             "https://www.ted.com/talks/chris_milk_how_virtual_reality_can_create_the_ultimate_empathy_machine",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/ChrisMilk_2015-480p.jpg?alt=media&token=dec395d4-b88e-46c3-b673-e5fbd6715ec2",
-            "https://firebasestorage.googleapis.com/v0/b/atv04-talks.appspot.com/" +
-                "o/2015-chris-milk-011-5000k.webm?alt=media&token=2738dc1e-c9f6-4927-982f-8910ac857e14",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "ChrisMilk_2015-480p.jpg?alt=media&token=78b01bc6-aa04-4f72-89f7-1ae90212b3a5",
+            "https://firebasestorage.googleapis.com/v0/b/padm-atv04-talks.appspot.com/o/" +
+                "ChrisMilk_2015-480p_small.webm?alt=media&token=4b5d85d4-19e7-4924-991c-632eebabba14",
         )
     )
 }
